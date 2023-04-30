b85bc4d1c85a73dec1b9f2272fdeedcd410a3c29
ALIF-PUTRA-CIRA <21839714-ALIF-PUTRA-CIRA@users.noreply.replit.com>
2023-04-30 09:29:59 +0000

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,359 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Title -->
+    <title>Alif's Portfolio</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="author" content="Brex-Theme" />
+    <!-- description -->
+    <meta
+      name="description"
+      content="Brex is a highly creative, modern, visually stunning and Bootstrap responsive multipurpose agency and portfolio HTML5 template."
+    />
+    <!-- favicon -->
+    <link rel="shortcut icon" href="img/logo.svg" />
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <!-- font-awesome icon -->
+    <link
+      href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+      rel="stylesheet"
+    />
+    <!-- Google Font -->
+    <link
+      href="https://fonts.googleapis.com/css?family=Muli:200,300,400,600,700,800,900"
+      rel="stylesheet"
+    />
+
+    <!-- animation -->
+    <link rel="stylesheet" href="css/plugins/animate.css" />
+    <!-- Animated Headlines -->
+    <link rel="stylesheet" href="css/plugins/animated-headlines.css" />
+    <!-- Main Style -->
+    <link rel="stylesheet" href="css/main.css" />
+  </head>
+
+  <body>
+    <!-- ===== Start Navbar ===== -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#home"
+          ><img src="img/logo.svg" alt=""
+        /></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#slider">HOME</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#about">ABOUT</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#services">SERVICES</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#contact">CONTACT</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- ===== End Navbar ===== -->
+
+    <!-- ===== Start Slider ===== -->
+    <section class="slider d-flex justify-content-center" id="slider">
+      <div class="container text-center">
+        <div class="row all-content">
+          <div class="col-12">
+            <div class="left-content">
+              <p class="hello">HELLO I'M</p>
+              <!-- Animated Text-->
+              <div class="animated-headlines">
+                <h1 class="cd-headline clip is-full-width">
+                  <span class="cd-words-wrapper">
+                    <b class="is-visible">Alif Putra Cira</b>
+                    <b>Junior programmer</b>
+                    <b>Junior analyst</b>
+                    <b>Human</b>
+                  </span>
+                </h1>
+              </div>
+              <p class="paragraph">
+                "The only way to do great work is to love what you do."
+                <br />
+                - Steve Jobs
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="particles-js" class="particels"></div>
+    </section>
+    <!-- ===== End Slider ===== -->
+
+    <!-- ===== Start About Me ===== -->
+    <section class="about-me" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-12">
+            <img src="img/self.png" alt="" class="img-fluid" />
+          </div>
+          <div class="col-lg-8 col-md-12 p-0">
+            <h2>About Me</h2>
+            <p>
+              Halo visitor, selamat datang di web saya ! <br />
+              Perkenalkan nama saya Alif Putra Cira. Saya adalah seorang
+              mahasiswa Universitas Sriwijaya berkelahiran 2003 dan saat ini
+              sedang menempuh studi di jurusan Sistem Informasi. Secara
+              personal, ada tiga hal yang sangat saya sukai yaitu game, musik,
+              dan terutama teknologi. Saya sangat tertarik dengan dunia
+              teknologi dan bagaimana teknologi dapat memberikan dampak positif
+              pada kehidupan sehari-hari. Selama masa kuliah, saya belajar
+              tentang berbagai konsep dasar seperti database, pemrograman, dan
+              sistem informasi. Saya sangat bersemangat untuk mengembangkan diri
+              dan belajar lebih banyak di bidang sistem informasi agar dapat
+              menjadi seorang profesional yang handal dan mampu memberikan
+              dampak positif bagi masyarakat melalui teknologi. <br />
+              <br />
+              <a href="#0">
+                <button>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span> Download CV
+                </button>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- ===== End About Me ===== -->
+
+    <!-- ===== Start Services ===== -->
+    <section class="services" id="services">
+      <h2 class="text-center">My Services</h2>
+      <div class="container">
+        <div class="row">
+          <!-- Box-1 -->
+          <div class="col-md-6 col-lg-4">
+            <div class="box">
+              <span class="fa fa-html5"></span>
+              <h3>HTML 5</h3>
+              <p>
+                HTML (Hypertext Markup Language) adalah bahasa markup yang
+                digunakan untuk membuat halaman web dan menentukan struktur dan
+                konten dari sebuah halaman web.
+              </p>
+            </div>
+          </div>
+          <!-- Box-2 -->
+          <div class="col-md-6 col-lg-4">
+            <div class="box">
+              <span class="fa fa-css3"></span>
+              <h3>CSS 3</h3>
+              <p>
+                CSS (Cascading Style Sheets) adalah sebuah bahasa pemrograman
+                yang digunakan untuk mengatur tampilan dari halaman web yang
+                dibuat dengan HTML (Hypertext Markup Language).
+              </p>
+            </div>
+          </div>
+          <!-- Box-3 -->
+          <div class="col-md-6 col-lg-4">
+            <div class="box">
+              <span class="fa fa-code"></span>
+              <h3>javascript</h3>
+              <p>
+                JavaScript adalah bahasa pemrograman yang sering digunakan untuk
+                memungkinkan interaksi pengguna yang dinamis dan responsif
+                dengan halaman web.
+              </p>
+            </div>
+          </div>
+          <!-- Box-4 -->
+          <div class="col-md-6 col-lg-4">
+            <div class="box">
+              <span class="fa fa-pie-chart"></span>
+              <h3>Bootstrap 5</h3>
+              <p>
+                Bootstrap 5 adalah sebuah framework yang menyediakan kumpulan
+                alat dan komponen yang siap digunakan untuk mempercepat
+                pengembangan web.
+              </p>
+              <p></p>
+            </div>
+          </div>
+          <!-- Box-5 -->
+          <div class="col-md-6 col-lg-4">
+            <div class="box">
+              <span class="fa fa-database"></span>
+              <h3>SQL</h3>
+              <p>
+                SQL (Structured Query Language) adalah sebuah bahasa pemrograman
+                khusus yang digunakan untuk mengelola dan memanipulasi database.
+              </p>
+            </div>
+          </div>
+          <!-- Box-6 -->
+          <div class="col-md-6 col-lg-4">
+            <div class="box">
+              <span class="fa fa-file-excel-o"></span>
+              <h3>Microsoft Excel</h3>
+              <p>
+                Microsoft Excel adalah program aplikasi lembar kerja elektronik
+                yang digunakan untuk mengelola, mengolah, dan menganalisis data
+                dalam bentuk tabel.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- ===== End Services ===== -->
+
+    <!-- ===== Start Contact ===== -->
+    <section class="contact" id="contact">
+      <h2 class="text-center">Get In Touch!</h2>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <form>
+              <div class="row">
+                <div class="col-sm-6">
+                  <input
+                    type="text"
+                    required
+                    class="form-control"
+                    placeholder="Name"
+                  />
+                </div>
+                <div class="col-sm-6">
+                  <input
+                    type="email"
+                    required
+                    class="form-control"
+                    placeholder="Email"
+                  />
+                </div>
+                <div class="col-sm-12">
+                  <input
+                    type="text"
+                    required
+                    class="form-control"
+                    placeholder="Subject"
+                  />
+                </div>
+              </div>
+              <div class="form-group">
+                <textarea
+                  required
+                  class="form-control"
+                  rows="5"
+                  id="comment"
+                  placeholder="Message"
+                ></textarea>
+              </div>
+              <button class="btn btn-block" type="submit">Send Now!</button>
+            </form>
+          </div>
+          <div class="col-md-4">
+            <div class="info">
+              <span class="fa fa-map-marker"></span>
+              <p>
+                Universitas Sriwijaya
+                <br />
+                Indralaya, Indonesia
+              </p>
+
+              <span class="fa fa-mobile"></span>
+              <p>
+                +62 821 8394 3782
+                <br />
+                +1 261 2671 389
+              </p>
+
+              <span class="fa fa-at"></span>
+              <p>
+                alif@email.com <br />
+                apusi.01@gmail.com
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- ===== End Contact ===== -->
+
+    <!-- ===== Start Footer ===== -->
+    <footer class="footer text-center">
+      <div class="social-icons">
+        <a
+          class="fa fa-facebook"
+          title="Alif Putra Cira"
+          href="https://www.facebook.com/alifputra.cira.1/"
+          aria-hidden="true"
+        ></a>
+        <a
+          class="fa fa-instagram"
+          title="@alifputra.c"
+          href="https://www.instagram.com/alifputra.c/"
+          aria-hidden="true"
+        ></a>
+        <a
+          class="fa fa-linkedin"
+          title="Alif Putra Cira"
+          href="https://id.linkedin.com/"
+          aria-hidden="true"
+        ></a>
+        <a
+          class="fa fa-whatsapp"
+          title="+6282163726606"
+          href="https://www.whatsapp.com/"
+          aria-hidden="true"
+        ></a>
+      </div>
+      <p>
+        All Right Resceved 2023 <br />
+        -apc
+      </p>
+    </footer>
+    <!-- ===== End Footer ===== -->
+
+    <!-- ==== javascript libraries ==== -->
+    <!-- jQuery-->
+    <script src="js/plugins/jquery-3.3.1.min.js"></script>
+    <!-- Bootstrap 4.1.3 -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/plugins/popper.min.js"></script>
+    <!-- Animated Headlines -->
+    <script src="js/plugins/animated-headlines.js"></script>
+    <!-- Nav jQuery-->
+    <script src="js/plugins/nav.jquery.js"></script>
+    <!-- Nav jQuery-->
+    <script src="js/plugins/jquery.stellar.js"></script>
+    <!-- Isotope -->
+    <script src="js/plugins/isotope.pkgd.min.js"></script>
+    <!-- Smooth-Scroll -->
+    <script src="js/smooth-scroll.js"></script>
+    <!-- Particles Js -->
+    <script src="js/plugins/particles.min.js"></script>
+    <script src="js/plugins/app.js"></script>
+    <!-- Animated Headlines -->
+    <script src="js/plugins/animated-headlines..j"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
