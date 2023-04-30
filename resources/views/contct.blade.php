5807e2ea44a52ebdc86978733ff58463882cb8dc
alifPc <101917083+alifPc@users.noreply.github.com>
2023-05-01 00:23:17 +0700

    Create contct.blade.php

---
+++ resources/views/contct.blade.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Title -->
+    <title>Alif's Portfolio</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="author" content="Brex-Theme" />
+    <!-- description -->
+    <meta
+      name="description"
+      content="Brex is a highly creative, modern, visually stunning and Bootstrap responsive multipurpose agency and portfolio HTML5 template."
+    />
+    <!-- favicon -->
+    <link rel="shortcut icon" href="img/logo.svg" />
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <!-- font-awesome icon -->
+    <link
+      href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+      rel="stylesheet"
+    />
+    <!-- Google Font -->
+    <link
+      href="https://fonts.googleapis.com/css?family=Muli:200,300,400,600,700,800,900"
+      rel="stylesheet"
+    />
+
+    <!-- animation -->
+    <link rel="stylesheet" href="css/plugins/animate.css" />
+    <!-- Animated Headlines -->
+    <link rel="stylesheet" href="css/plugins/animated-headlines.css" />
+    <!-- Main Style -->
+    <link rel="stylesheet" href="css/main.css" />
+  </head>
+
+  <body>
+    <!-- ===== Start Navbar ===== -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="#home"
+          ><img src="img/logo.svg" alt=""
+        /></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">HOME</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">ABOUT</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">SERVICES</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#contact">CONTACT</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- ===== End Navbar ===== -->
+
+    <!-- ===== Start Contact ===== -->
+    <section class="contact" id="contact">
+      <h2 class="text-center">Get In Touch!</h2>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <form>
+              <div class="row">
+                <div class="col-sm-6">
+                  <input
+                    type="text"
+                    required
+                    class="form-control"
+                    placeholder="Name"
+                  />
+                </div>
+                <div class="col-sm-6">
+                  <input
+                    type="email"
+                    required
+                    class="form-control"
+                    placeholder="Email"
+                  />
+                </div>
+                <div class="col-sm-12">
+                  <input
+                    type="text"
+                    required
+                    class="form-control"
+                    placeholder="Subject"
+                  />
+                </div>
+              </div>
+              <div class="form-group">
+                <textarea
+                  required
+                  class="form-control"
+                  rows="5"
+                  id="comment"
+                  placeholder="Message"
+                ></textarea>
+              </div>
+              <button class="btn btn-block" type="submit">Send Now!</button>
+            </form>
+          </div>
+          <div class="col-md-4">
+            <div class="info">
+              <span class="fa fa-map-marker"></span>
+              <p>
+                Universitas Sriwijaya
+                <br />
+                Indralaya, Indonesia
+              </p>
+
+              <span class="fa fa-mobile"></span>
+              <p>
+                +62 821 8394 3782
+                <br />
+                +1 261 2671 389
+              </p>
+
+              <span class="fa fa-at"></span>
+              <p>
+                alif@email.com <br />
+                apusi.01@gmail.com
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- ===== End Contact ===== -->
+
+    <!-- ===== Start Footer ===== -->
+    <footer class="footer text-center">
+      <div class="social-icons">
+        <a
+          class="fa fa-facebook"
+          title="Alif Putra Cira"
+          href="https://www.facebook.com/alifputra.cira.1/"
+          aria-hidden="true"
+        ></a>
+        <a
+          class="fa fa-instagram"
+          title="@alifputra.c"
+          href="https://www.instagram.com/alifputra.c/"
+          aria-hidden="true"
+        ></a>
+        <a
+          class="fa fa-linkedin"
+          title="Alif Putra Cira"
+          href="https://id.linkedin.com/"
+          aria-hidden="true"
+        ></a>
+        <a
+          class="fa fa-whatsapp"
+          title="+6282163726606"
+          href="https://www.whatsapp.com/"
+          aria-hidden="true"
+        ></a>
+      </div>
+      <p>
+        All Right Resceved 2023 <br />
+        -apc
+      </p>
+    </footer>
+    <!-- ===== End Footer ===== -->
+
+    <!-- ==== javascript libraries ==== -->
+    <!-- jQuery-->
+    <script src="js/plugins/jquery-3.3.1.min.js"></script>
+    <!-- Bootstrap 4.1.3 -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/plugins/popper.min.js"></script>
+    <!-- Animated Headlines -->
+    <script src="js/plugins/animated-headlines.js"></script>
+    <!-- Nav jQuery-->
+    <script src="js/plugins/nav.jquery.js"></script>
+    <!-- Nav jQuery-->
+    <script src="js/plugins/jquery.stellar.js"></script>
+    <!-- Isotope -->
+    <script src="js/plugins/isotope.pkgd.min.js"></script>
+    <!-- Smooth-Scroll -->
+    <script src="js/smooth-scroll.js"></script>
+    <!-- Particles Js -->
+    <script src="js/plugins/particles.min.js"></script>
+    <script src="js/plugins/app.js"></script>
+    <!-- Animated Headlines -->
+    <script src="js/plugins/animated-headlines..j"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
